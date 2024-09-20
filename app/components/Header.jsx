256c3f0e40d7115ac09087b02a310b85aea603c4
Jaimin Moradiya <jaiminmoradiya23@gmail.com>
2024-09-20 18:33:45 +0530

    initialize eslint and fonts

---
+++ app/components/Header.jsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex items-center">
+      <div className="max-w-[275px] w-full">Current Page</div>
+      <div className="grow">Jaimin</div>
+      <div className="max-w-[275px] w-full">Menu</div>
+    </div>
+  );
+};
+
+export default Header;
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
+import React from "react";
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "./components/Header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -19,10 +21,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    >
+      <body>
+        <Header />
         {children}
       </body>
     </html>
